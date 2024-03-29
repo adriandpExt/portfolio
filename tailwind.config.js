a926944ddb984ad1a7975f048e3ea56d16bbd34c
adriandp52 <adriandp52@gmail.com>
2024-03-29 11:48:59 +0800

    add: additional package and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "className",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          // Define your light mode colors here
+          primary: "#ffffff",
+          secondary: "#f3f4f6",
+          text: "#1f2937",
+        },
+        dark: {
+          // Define your dark mode colors here
+          primary: "#1f2937",
+          secondary: "#111827",
+          text: "#f9fafb",
+        },
+      },
+      fontFamily: {
+        dance: ["Dance\\Script"],
+      },
+    },
   },
   plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+      // Specify symbolId format
+      symbolId: "icon-[dir]-[name]",
+    }),
+    react(),
+  ],
   base: "/portfolio/",
 });
